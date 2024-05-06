a7807419dddde51534d052ab4366a62de7b06297
Justin Hoffman <justin@ohmedia.ca>
2024-05-06 15:24:04 -0600

    entity/form fields

--- src/Entity/Logo.php
+++ src/Entity/Logo.php
@@ -2,7 +2,10 @@
 
 namespace OHMedia\LogoBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use OHMedia\FileBundle\Entity\File;
 use OHMedia\LogoBundle\Repository\LogoRepository;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
 
@@ -16,13 +19,94 @@ class Logo
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $url = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?File $image = null;
+
+    /**
+     * @var Collection<int, LogoGroup>
+     */
+    #[ORM\ManyToMany(targetEntity: LogoGroup::class)]
+    private Collection $groups;
+
+    public function __construct()
+    {
+        $this->groups = new ArrayCollection();
+    }
+
     public function __toString(): string
     {
-        return 'Logo #'.$this->id;
+        return $this->name;
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getImage(): ?File
+    {
+        return $this->image;
+    }
+
+    public function setImage(File $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, LogoGroup>
+     */
+    public function getGroups(): Collection
+    {
+        return $this->groups;
+    }
+
+    public function addGroup(LogoGroup $group): static
+    {
+        if (!$this->groups->contains($group)) {
+            $this->groups->add($group);
+        }
+
+        return $this;
+    }
+
+    public function removeGroup(LogoGroup $group): static
+    {
+        $this->groups->removeElement($group);
+
+        return $this;
     }
 }
--- src/Entity/LogoGroup.php
+++ src/Entity/LogoGroup.php
@@ -2,6 +2,8 @@
 
 namespace OHMedia\LogoBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\LogoBundle\Repository\LogoGroupRepository;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
@@ -16,13 +18,63 @@ class LogoGroup
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    /**
+     * @var Collection<int, Logo>
+     */
+    #[ORM\ManyToMany(targetEntity: Logo::class)]
+    private Collection $logos;
+
+    public function __construct()
+    {
+        $this->logos = new ArrayCollection();
+    }
+
     public function __toString(): string
     {
-        return 'Group #'.$this->id;
+        return $this->title;
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Logo>
+     */
+    public function getLogos(): Collection
+    {
+        return $this->logos;
+    }
+
+    public function addLogo(Logo $logo): static
+    {
+        if (!$this->logos->contains($logo)) {
+            $this->logos->add($logo);
+        }
+
+        return $this;
+    }
+
+    public function removeLogo(Logo $logo): static
+    {
+        $this->logos->removeElement($logo);
+
+        return $this;
     }
 }
